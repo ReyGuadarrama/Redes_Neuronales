85c953580d66cb2c8f7fbd6ebeee108e66060ee0
ReyGuadarrama <88348141+ReyGuadarrama@users.noreply.github.com>
2023-10-11 09:38:18 -0600

    Delete 02_Normalizacion/Regresión_múltiple directory

--- 02_Normalizacion/Regresión_múltiple/costo.m
+++
@@ -1,3 +0,0 @@
-function C = costo(w, x, T)
-    C = ((hipotesis(w, x) - T).^2) * ones(length(x), 1);
-end
--- 02_Normalizacion/Regresión_múltiple/costo_n1.m
+++
@@ -1,5 +0,0 @@
-
-function C = costo_n1(w, x, T)
-    C = ((hipotesis_n1(w, x) - T).^2) * ones(length(x), 1);
-end
-    
