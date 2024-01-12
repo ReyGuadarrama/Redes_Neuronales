c9f0a731b8a4c1e180dc278c3600cf553cc550e2
ReyGuadarrama <88348141+ReyGuadarrama@users.noreply.github.com>
2024-01-11 21:15:46 -0600

    Delete redes_de_regresion directory

--- redes_de_regresion/neurona.m
+++
@@ -1,8 +0,0 @@
-function z = neurona(w, x)
-
-    % Calcula el polinomio de salida de una neurona
-    z = x' * w;
-
-    % Devuelve una matriz m x n donde m es el numero de muestras y n el
-    % numero de neuronas
-end
