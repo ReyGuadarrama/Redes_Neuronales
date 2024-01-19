7fe56b90919e96325bfbd800f7859f8917ff4918
ReyGuadarrama <88348141+ReyGuadarrama@users.noreply.github.com>
2024-01-18 23:17:27 -0600

    Delete modelo_red directory

--- modelo_red/neurona.m
+++
@@ -1,8 +0,0 @@
-function z = neurona(w, x, b)
-
-    % Calcula el polinomio de salida de una neurona
-    z = (w' * x) + b;
-
-    % Devuelve una matriz m x n donde m es el numero de muestras y n el
-    % numero de neuronas
-end
