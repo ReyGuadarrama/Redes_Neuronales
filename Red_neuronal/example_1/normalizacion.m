d5c4a6c9ead1b93e520b542e51e35866fa568a6f
ReyGuadarrama <88348141+ReyGuadarrama@users.noreply.github.com>
2024-03-22 11:19:49 -0600

    Delete Red_neuronal directory

--- Red_neuronal/example_1/normalizacion.m
+++
@@ -1,9 +0,0 @@
-function N = normalizacion(x)
-
-    maximos = max(x, [], 2);
-    minimos = min(x, [], 2);
-    rango = maximos - minimos;
-    unit = (x - minimos) ./ rango;
-    N = (unit - 0.5) * 2;
-
-end
